c9a20cc0aa41cc9d676182435b625d21e28c6303
SayandebB <sayandeb.banerjee@gmail.com>
2015-03-25 10:20:20 +0530

    R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Thsi code consists of 2 functions: makeCacheMatrix and cacheSolve
+## makeCacheMatrix takes an invertible matrix and returns 4 functions to 
+## cache oeprations to get the matrix, get its inverse, return the matrix
+## and return its inverse.
+## cacheSolve uses functions returned by the above and picks up the matrix, 
+## then calculates its inverse and returns the invers
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function to return 4 functions to set and return matrix n its inverse
+
+makeCacheMatrix<-function(xMat)
+{
+	if(!is.matrix(xMat)) print("You must enter a matrix as argument")
+	else
+	{
+		set<-function(yMat)
+		{
+			xMat<<-yMat
+			invMat<<-NULL
+		}
+		get<-function() xMat
+		setInv<-function(invOfMat) invMat<<-invOfMat
+		getInv<-function() invMat
+
+		list(set=set,get=get,setInv=setInv, getInv=getInv)
+	}
 
 }
 
 
-## Write a short comment describing this function
+## Function to calculate the inverse of matrix and return it 
+## using makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mat, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+	dataMat<-mat$get()
+	invOfMat<-solve(dataMat)
+	mat$setInv(invOfMat)
+	invOfMat
+
+
+
 }
